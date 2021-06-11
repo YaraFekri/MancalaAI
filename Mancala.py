902ec003ce5dc6145287d81867ee4d16518188f2
HajarRizk <58341522+HajarRizk@users.noreply.github.com>
2021-06-12 01:25:21 +0200

    storeVerbose added

--- Mancala.py
+++ Mancala.py
@@ -19,3 +19,19 @@ def printgame(game):
     for i in range(0, 6, 1):
         print('  ', game[i], '   ', end='')
     print('\n')
+    
+def storeVerbose(i):
+    try:
+        global num_of_nodes
+        verbose['AverageBF'] = (verbose['NumofLeaf'] + num_of_nodes)/(num_of_nodes+1)
+        verbose_file = open('verbose.txt', 'a')
+        verbose_file.write('Turn' + str(i) + ':  ' + str(verbose) + '\n')
+        verbose_file.close()
+        verbose['AverageBF'] = 0
+        verbose['NumofLeaf'] = 0
+        verbose['values'].clear()
+        verbose['numofCut'] = 0
+        verbose['levels'].clear()
+        num_of_nodes = 0
+    except:
+        print("Unable to write to file")
